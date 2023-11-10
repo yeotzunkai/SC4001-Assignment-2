4bb66e07f469e51063a507b1d3085cecd8728357
Yeo Tzun Kai <yeotzunkai@gmail.com>
2023-11-11 00:33:01 +0800

    Update Deform.py

--- Testing Fix/Deform.py
+++ Testing Fix/Deform.py
@@ -1,3 +1,26 @@
+import numpy as np
+import torch
+from torch import nn
+from torchvision import models
+import pytorch_lightning as pl
+from torchmetrics import Accuracy
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning.trainer import Trainer
+import torch.nn.functional as F
+
+import torchvision
+from torchvision.ops import DeformConv2d
+
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+import os
+import sys
+
+from IPython.core.display import display, HTML
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,66 +32,135 @@ from sklearn.preprocessing import LabelEncoder
 import torchvision
 from tqdm import tqdm  # Standard import for scripts and applications
 import pandas as pd
-class DeformableCNNModel(nn.Module):
-    def __init__(self):
-        super(DeformableCNNModel, self).__init__()
+class ResBlock(nn.Module):
+    def init(self, in_channels, out_channels, downsample):
+        super(ResBlock, self).init()
+        stride = 2 if downsample else 1
 
-        # Convolutional weights
-        self.weight1 = nn.Parameter(torch.Tensor(32, 3, 3, 3))
-        self.weight2 = nn.Parameter(torch.Tensor(64, 32, 3, 3))
-        self.weight3 = nn.Parameter(torch.Tensor(128, 64, 3, 3))
-        nn.init.kaiming_uniform_(self.weight1, nonlinearity='relu')
-        nn.init.kaiming_uniform_(self.weight2, nonlinearity='relu')
-        nn.init.kaiming_uniform_(self.weight3, nonlinearity='relu')
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+            nn.BatchNorm2d(out_channels)
+        ) if downsample else nn.Sequential()
 
-        # Bias terms
-        self.bias1 = nn.Parameter(torch.Tensor(32))
-        self.bias2 = nn.Parameter(torch.Tensor(64))
-        self.bias3 = nn.Parameter(torch.Tensor(128))
-        self.bias1.data.fill_(0)
-        self.bias2.data.fill_(0)
-        self.bias3.data.fill_(0)
-
-        # Offset for deformable convolution
-        self.offsets1 = nn.Conv2d(3, 2 * 3 * 3, kernel_size=3, padding=1, stride=1)
-        self.offsets2 = nn.Conv2d(32, 2 * 3 * 3, kernel_size=3, padding=1, stride=1)
-        self.offsets3 = nn.Conv2d(64, 2 * 3 * 3, kernel_size=3, padding=1, stride=1)
-
-        # Pooling layer
-        self.pool = nn.MaxPool2d(2, 2)
-
-        # Dummy forward pass to determine the size for the fully connected layer
-        dummy_input = torch.zeros(1, 3, 299, 299)  # Adjust the size based on your input image size
-        fc_size = self._get_conv_output(dummy_input)
-
-        # Fully connected layers
-        self.fc1 = nn.Linear(fc_size, 512)
-        self.fc2 = nn.Linear(512, 102)
-        self.dropout = nn.Dropout(0.2)
-        self.relu = nn.ReLU()
-
-    def _get_conv_output(self, x):
-        x = torchvision.ops.deform_conv2d(x, self.offsets1(x), self.weight1, self.bias1, padding=1)
-        x = self.pool(x)
-        x = torchvision.ops.deform_conv2d(x, self.offsets2(x), self.weight2, self.bias2, padding=1)
-        x = self.pool(x)
-        x = torchvision.ops.deform_conv2d(x, self.offsets3(x), self.weight3, self.bias3, padding=1)
-        return int(torch.prod(torch.tensor(x.size()[1:])))
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
-        x = torchvision.ops.deform_conv2d(x, self.offsets1(x), self.weight1, self.bias1, padding=1)
-        x = self.relu(x)
-        x = self.pool(x)
-        x = torchvision.ops.deform_conv2d(x, self.offsets2(x), self.weight2, self.bias2, padding=1)
-        x = self.relu(x)
-        x = self.pool(x)
-        x = torchvision.ops.deform_conv2d(x, self.offsets3(x), self.weight3, self.bias3, padding=1)
-        x = self.relu(x)
-        x = x.view(x.size(0), -1)
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
+        shortcut = self.shortcut(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = x + shortcut
+        return F.relu(x)
+
+class DeformResNet(nn.Module):
+    def init(self, in_channels, resblock, outputs=2):
+        super(DeformResNet, self).init()
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+
+        self.layers = nn.ModuleList([
+            nn.Sequential(resblock(64, 64, downsample=False), resblock(64, 64, downsample=False)),
+            nn.Sequential(resblock(64, 128, downsample=True), resblock(128, 128, downsample=False)),
+            nn.Sequential(resblock(128, 256, downsample=True), resblock(256, 256, downsample=False)),
+            nn.Sequential(resblock(256, 512, downsample=True), resblock(512, 512, downsample=False)),
+        ])
+
+        self.deforms = nn.ModuleList([
+            DeformConv2d(64, 64, 3, padding=1),
+            DeformConv2d(128, 128, 3, padding=1),
+            DeformConv2d(256, 256, 3, padding=1),
+            DeformConv2d(512, 512, 3, padding=1),
+        ])
+
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(512, outputs)
+
+    def forward(self, x):
+        x = self.layer0(x)
+        for layer, deform in zip(self.layers, self.deforms):
+            offset = torch.rand(x.size(0), 2 * 3 * 3, x.size(2), x.size(3))
+            if torch.cuda.is_available():
+                offset = offset.to(x.device)
+            x = deform(layer(x), offset)
+        x = self.gap(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc(x)
         return x
+
+class LitDeformModel(pl.LightningModule):
+    def init(self, n_classes=2):
+        super(LitDeformModel, self).init()
+        self.model = DeformResNet(3, ResBlock, 2)
+        self.opt_params = {"lr": 0.001}
+
+    def forward(self, x):
+        return self.model(x)
+# class DeformableCNNModel(nn.Module):
+#     def __init__(self):
+#         super(DeformableCNNModel, self).__init__()
+#
+#         # Convolutional weights
+#         self.weight1 = nn.Parameter(torch.Tensor(32, 3, 3, 3))
+#         self.weight2 = nn.Parameter(torch.Tensor(64, 32, 3, 3))
+#         self.weight3 = nn.Parameter(torch.Tensor(128, 64, 3, 3))
+#         nn.init.kaiming_uniform_(self.weight1, nonlinearity='relu')
+#         nn.init.kaiming_uniform_(self.weight2, nonlinearity='relu')
+#         nn.init.kaiming_uniform_(self.weight3, nonlinearity='relu')
+#
+#         # Bias terms
+#         self.bias1 = nn.Parameter(torch.Tensor(32))
+#         self.bias2 = nn.Parameter(torch.Tensor(64))
+#         self.bias3 = nn.Parameter(torch.Tensor(128))
+#         self.bias1.data.fill_(0)
+#         self.bias2.data.fill_(0)
+#         self.bias3.data.fill_(0)
+#
+#         # Offset for deformable convolution
+#         self.offsets1 = nn.Conv2d(3, 2 * 3 * 3, kernel_size=3, padding=1, stride=1)
+#         self.offsets2 = nn.Conv2d(32, 2 * 3 * 3, kernel_size=3, padding=1, stride=1)
+#         self.offsets3 = nn.Conv2d(64, 2 * 3 * 3, kernel_size=3, padding=1, stride=1)
+#
+#         # Pooling layer
+#         self.pool = nn.MaxPool2d(2, 2)
+#
+#         # Dummy forward pass to determine the size for the fully connected layer
+#         dummy_input = torch.zeros(1, 3, 299, 299)  # Adjust the size based on your input image size
+#         fc_size = self._get_conv_output(dummy_input)
+#
+#         # Fully connected layers
+#         self.fc1 = nn.Linear(fc_size, 512)
+#         self.fc2 = nn.Linear(512, 102)
+#         self.dropout = nn.Dropout(0.2)
+#         self.relu = nn.ReLU()
+#
+#     def _get_conv_output(self, x):
+#         x = torchvision.ops.deform_conv2d(x, self.offsets1(x), self.weight1, self.bias1, padding=1)
+#         x = self.pool(x)
+#         x = torchvision.ops.deform_conv2d(x, self.offsets2(x), self.weight2, self.bias2, padding=1)
+#         x = self.pool(x)
+#         x = torchvision.ops.deform_conv2d(x, self.offsets3(x), self.weight3, self.bias3, padding=1)
+#         return int(torch.prod(torch.tensor(x.size()[1:])))
+#
+#     def forward(self, x):
+#         x = torchvision.ops.deform_conv2d(x, self.offsets1(x), self.weight1, self.bias1, padding=1)
+#         x = self.relu(x)
+#         x = self.pool(x)
+#         x = torchvision.ops.deform_conv2d(x, self.offsets2(x), self.weight2, self.bias2, padding=1)
+#         x = self.relu(x)
+#         x = self.pool(x)
+#         x = torchvision.ops.deform_conv2d(x, self.offsets3(x), self.weight3, self.bias3, padding=1)
+#         x = self.relu(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.relu(self.fc1(x))
+#         x = self.dropout(x)
+#         x = self.fc2(x)
+#         return x
 ## Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 '''
@@ -120,7 +212,7 @@ with open('label_encoder.pkl', 'wb') as file:
 
 
 # Initialize Inception V3 with pre-trained weights
-model = DeformableCNNModel().to(device)
+model = LitDeformModel().to(device)
 
 
 # Loss function and optimizer
